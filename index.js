64b97d94da0859ae000c1c9cea05553959fb7b5d
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-03-13 12:16:01 +0530

    trst

--- index.js
+++ index.js
@@ -38,11 +38,36 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
+const data = require("./data.json");
+
 app.use(express.static('static'));
+
+app.use(express.json());''
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+app.post("/students/above-threshold",(req,res)=>{
+  try {
+    const {threshold} = req.body;
+    if (!threshold) {
+      return res.status(401).send({msg:"Please provide threshold"})
+    }
+    const studentData = data.map((ele)=>{
+      return {name:ele.name,total:ele.total};
+    }).filter((ele)=>{
+
+      return ele.total>threshold;
+    })
+
+    return res.status(200).send({count:studentData.length,student:studentData});
+
+
+  } catch(error){
+    return res.status(500).send({msg:"Something went wrong",error})
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
